ca5f0a9dde8be0bf52c061159addb1d58f9ca91d
mohammad faozul azim rafi <rafi@dwetech.com>
2014-09-16 23:54:09 +0600

    just created the app

---
+++ spreadsheet.js
@@ -0,0 +1,24 @@
+/**
+ * SpreadSheet.js
+ *
+ * SpreadSheet plugin using jQuery
+ * Author: Md Faozul Azim Rafi (me@rafi.pro)
+ * Authhor: Md Ibrahim Islam (ibm@cyberdude.com)
+ *
+ */
+var spreadSheet = spreadSheet || {
+
+
+	options: {
+		var1: 'someValue1',
+		var2: 'someValue2'
+	}
+
+
+
+
+	/**** Initializes the actual app ****/
+	init: function() {
+
+	}
+}
